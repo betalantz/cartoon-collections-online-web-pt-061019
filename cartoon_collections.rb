5de67fdf7a2b864fedbdcdebcffa700f9c603c0c
Lantz Warrick <betalantz@gmail.com>
2019-06-24 04:32:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,26 +1,16 @@
-def roll_call_dwarves(dwarves)
-  dwarves.each.with_index(1) {|dwarf, idx| puts "#{idx}. #{dwarf}"}
+def roll_call_dwarves# code an argument here
+  # Your code here
 end
 
-def summon_captain_planet(arr)
-  arr.map do |ele|
-    ele[0] = ele[0].upcase
-    ele << "!"
-  end
+def summon_captain_planet# code an argument here
+  # Your code here
 end
 
-def long_planeteer_calls(calls)
-  calls.map do |call|
-    if call.length > 4
-      return true
-    else
-      return false
-    end
+def long_planeteer_calls# code an argument here
+  # Your code here
 end
 
-def find_the_cheese(snacks)
-  snacks.find do |snack|
-    cheese_types.include?(snack)
-    cheese_types = ["cheddar", "gouda", "camembert"]
-  end
+def find_the_cheese# code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
 end
