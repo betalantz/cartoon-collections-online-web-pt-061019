4cf1fabee6b78ceca3f529a4aff94ee5598fac3f
Lantz Warrick <betalantz@gmail.com>
2019-06-24 04:38:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,10 @@ def long_planeteer_calls(calls)
   calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find do |snack|
+    cheese_types.includes?(snack)
+  end
 end
